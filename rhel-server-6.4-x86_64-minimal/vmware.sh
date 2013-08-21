699bfb49b17cbe03879966e7080f65125870bef5
Sam Kuonen <sam.kuonen@oit.gatech.edu>
2013-08-21 13:51:10 -0400

    Moved perl to ks.cfg Added vmware-tools-esx-kmods

--- rhel-server-6.4-x86_64-minimal/vmware.sh
+++ rhel-server-6.4-x86_64-minimal/vmware.sh
@@ -1,3 +1,3 @@
 yum -y install http://packages.vmware.com/tools/esx/latest/repos/vmware-tools-repo-RHEL6-9.0.5-1.el6.x86_64.rpm
 
-yum -y install perl vmware-tools-esx-nox
+yum -y install vmware-tools-esx-kmods vmware-tools-esx-nox
